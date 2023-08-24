2830fe4810f07d88944987e7eea07bb5bb361467
Sheizamm <f20201581@goa.bits-pilani.ac.in>
2023-08-25 04:42:46 +0530

    Initial My Application Project

--- app/src/main/java/com/example/java/greetings/MainActivity.kt
+++ app/src/main/java/com/example/java/greetings/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -11,6 +12,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.java.greetings.ui.theme.GreetingsTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,10 +31,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    Surface(color = Color.Yellow) {
+        Text(
             text = "Hello $name!",
-            modifier = modifier
-    )
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
